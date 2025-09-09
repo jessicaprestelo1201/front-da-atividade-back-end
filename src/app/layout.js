e59425af7d125921a528a7d40983ff7f49f68c6b
jessicaprestelo1201 <jessica.p.jesus@aluno.senai.br>
2025-09-09 15:39:37 -0300

    feat: alteração no nome da página

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-    title: "escreva o TÍTULO para aparecer na aba do navegador",
+    title: "Lana Del Rey",
     description: "Projeto pra mostrar tudo que eu sei",
     icons: {
         icon: "/icons/favicon.ico",
